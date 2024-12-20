653d6feb6c6ad87c702c86610c87cec701c7aa38
AV306 <brandonjoash@gmail.com>
2024-12-20 18:21:36 +0800

    e

--- src/main/java/me/av306/keybindsgaloreplus/KeybindsGalorePlus.java
+++ src/main/java/me/av306/keybindsgaloreplus/KeybindsGalorePlus.java
@@ -7,10 +7,12 @@ import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.ClickEvent;
 import net.minecraft.text.Text;
 
 import java.io.IOException;
 
+import net.minecraft.util.Formatting;
 import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -42,7 +44,11 @@ public class KeybindsGalorePlus implements ClientModInitializer
                 null
             );
 
-            LOGGER.info( KeybindSelectorScreen.SKIPPED_KEYS.getClass().getName() );
+            // There's no good, easy way to enable DEBUG level so I'm just gonna
+            // cram a bunch of if statements around
+            LOGGER.info( "Debug mode: {}", KeybindSelectorScreen.DEBUG );
+
+            //LOGGER.info( KeybindSelectorScreen.SKIPPED_KEYS.getClass().getName() );
 
             // Read custom data
             customDataManager = new DataManager(
@@ -71,6 +77,20 @@ public class KeybindsGalorePlus implements ClientModInitializer
                     {
                         //client.player.sendMessage( Text.translatable( "text.keybindsgaloreplus.configreloadfail" ) );
                         client.player.sendMessage( Text.translatable( "text.keybindsgaloreplus.configreloadfail" ), false );
+                        try
+                        {
+                            configManager.checkConfigFile();
+                        }
+                        catch ( IOException ioe2 )
+                        {
+                            client.player.sendMessage(
+                                    Text.translatable( "text.keybindsgaloreplus.configreloadfailagain" ).append(
+                                            Text.literal( "https://github.com/AV306/KeybindsGalore-Plus/blob/master/src/main/resources/keybindsgaloreplus_config.properties" )
+                                                    .formatted( Formatting.YELLOW )
+                                                    .styled( style -> style.withClickEvent( new ClickEvent( ClickEvent.Action.OPEN_URL, "https://github.com/AV306/KeybindsGalore-Plus/blob/master/src/main/resources/keybindsgaloreplus_config.properties" ) ) )
+                                    )
+                            );
+                        }
                         return;
                     }
 
