e325b4312798662c0fa6a49ae13f394eb5c76584
diblenderbenzene <brandonjoash@gmail.com>
2025-04-28 05:59:20 +0000

    1.20-1.20.1-specific changes

--- src/main/java/me/av306/keybindsgaloreplus/KeybindSelectorScreen.java
+++ src/main/java/me/av306/keybindsgaloreplus/KeybindSelectorScreen.java
@@ -89,8 +89,8 @@ public class KeybindSelectorScreen extends Screen
     {
         // ===== Version dependent =====
         //super.render( context, mouseX, mouseY, delta );
-        this.renderBackground( context, mouseX, mouseY, delta );
-        //this.renderBackground( context );
+        //this.renderBackground( context, mouseX, mouseY, delta );
+        this.renderBackground( context );
 
         // Pixel coords of screen centre
         // Only set these on the first frame
@@ -430,29 +430,33 @@ public class KeybindSelectorScreen extends Screen
 
 
     //* >=1.20.2
-    @Override
-    public void renderBackground( DrawContext context, int mouseX, int mouseY, float delta )
-    {
-        // Remove the darkened background if needed
-        // This can help performance, as with all post-processing
-        if ( Configurations.DARKENED_BACKGROUND )
-        {
-            // Don't crash on 1.20.1 because I REALLY don't want another branch
-            // TODO: maybe we can use reflection to call the correct method?
-            try { super.renderBackground( context, mouseX, mouseY, delta ); } //* >=1.20.2
-            catch ( NoSuchMethodException | NoSuchMethodError e )
-            {}
-        }
-    }
+    // @Override
+    // public void renderBackground( DrawContext context, int mouseX, int mouseY, float delta )
+    // {
+    //     // Remove the darkened background if needed
+    //     // This can help performance, as with all post-processing
+    //     if ( Configurations.DARKENED_BACKGROUND )
+    //     {
+    //         // Don't crash on 1.20.1 because I REALLY don't want another branch
+    //         // TODO: maybe we can use reflection to call the correct method?
+    //         try { super.renderBackground( context, mouseX, mouseY, delta ); } //* >=1.20.2
+    //         catch ( NoSuchMethodException | NoSuchMethodError e )
+    //         {}
+    //     }
+    // }
 
     //* <1.20.2
     // Annoyingly, we can have the method in >1.20.2 but not the super call :(
-    //@Override
-    // public void renderBackground( DrawContext context ) //* <1.20.2
-    // {
-    //     //* // ===== Version dependent =====
-    //     // Remove the darkened background if needed
-    //     // This can help performance, as with all post-processing
-    //     if ( Configurations.DARKENED_BACKGROUND ) super.renderBackground( context ); //* <1.20.2
-    // }
+    @Override
+    public void renderBackground( DrawContext context ) //* <1.20.2
+    {
+        //* // ===== Version dependent =====
+
+        // Catch
+        if ( Configurations.DARKENED_BACKGROUND ) 
+        {
+            try { super.renderBackground( context ); }
+            catch ( NoSuchMethodError e ) {}
+        }
+    }
 }
